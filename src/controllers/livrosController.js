c26c0d58d279063bd2ccf1b9c7f56ef2f6965943
Pedro Lucas <pedro.souza@getinfo.tech>
2022-03-21 00:03:30 -0300

    add: CRUD feito com mongodb

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,3 +1,4 @@
+import { response } from "express"
 import livros from "../models/Livro.js"
 
 class LivroController{
@@ -5,9 +6,57 @@ class LivroController{
     static listarLivros = (req, res) => {
         livros.find((err, livros)=>{
             res.status(200).json(livros)
-
     })
     }
+    static listarLivroPorId = (req, res) =>{
+        const id = req.params.id;
+        livros.findById(id, (err, livros)=>{
+            if(err){
+                res.status(400).send({message: err.message + 'id do livro não localizado'})
+            }else{
+                res.status(200).send(livros)
+            }
+        })
+    }
+    static cadastrarLivro = (req, res) =>{
+        try{
+            let livro = new livros(req.body)
+
+            livro.save()
+            return res.status(201).json(livro)
+        }
+        catch(e){
+            return res.status(500).json('Error')
+        }
+        
+    }
+    static atualizarLivro = (req, res)=>{
+        const id = req.params.id
+
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err)=>{
+            if(!err){
+                res.status(200).send({message: 'Livro atualizado com sucesso!'})
+            }else{
+                res.status(500).send({message: err.message})
+
+            }
+        })
+    }
+    static excluirLivro = (req,res) =>{
+        const id = req.params.id
+
+        livros.findByIdAndDelete(id,(err)=>{
+            if(err){
+                res.status(400).send({message: err.message})
+            }else{
+                res.status(200).send({message: 'Livro removido com sucesso!'})
+            }
+
+        })
+        
+
+        }
+    
 }
 
 export default LivroController;
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,9 +4,9 @@ const livroSchema = new mongoose.Schema(
     {
         id: {type: String},
         titulo: {type: String, required: true},
-        autror: {type: String, required: true},
+        autor: {type: String, required: true},
         editora: {type: String, required: true},
-        numeroPagaginas : {type: Number}
+        numeroPaginas : {type: Number}
 
     }
 
